9513828d8846497fbfe5ea125207685ed3702575
Thanh Luong <thanh@gotitapp.co>
2018-11-01 12:33:18 +0700

    Thanh started

--- first_example.py
+++ first_example.py
@@ -1,3 +1,16 @@
 if __name__ == '__main__':
-    a = "Hello World"
-    print(a)
+    a = "welcome abc"
+    b = a.split()
+    print(b)
+    # a = 3
+    # b = 2
+    # if a > b:
+    #     print("a is greater than b")
+    # elif a < b:
+    #     print("a is smaller than b")
+    # else:
+    #     print("a is equal b")
+# viet mot chuowng trinh tinh tong 2 so va print
+
+
+
