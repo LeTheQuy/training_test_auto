af52ff30b25c2b5cdf7e0d0aae88398d5353938e
Got It! Inc <thanhhuyenk59uet@gmail.com>
2018-11-06 16:49:38 +0700

    day 1 homework

--- Sam_test_auto/day1.py
+++ Sam_test_auto/day1.py
@@ -1,10 +1,15 @@
 # Bài tập sẽ là: Viết chương trình nhập vào một số từ bàn phím và in ra số đó là số âm hay số dương.
 # how to input from keyboard in python
 
-number = int(input("Input a number: "))
-if number == 0:
-    print("So 0")
-elif number > 0:
-    print("So duong")
-else:
-    print("So am")
+user_input = input("Input a number: ")
+
+try:
+    val = int(user_input)
+    if val == 0:
+        print(str(val) + " is equal to 0")
+    elif val > 0:
+        print(str(val) + " is greater than 0")
+    else:
+        print(str(val) + " is smaller than 0")
+except ValueError:
+    print("That's not an int!")
