de4ddf664cf4bc8518d5dcadb1b2d8b3e769cab2
Thanh Luong <thanh@gotitapp.co>
2018-11-06 20:08:48 +0700

    Thanh day 1 homework

--- first_example.py
+++ first_example.py
@@ -1,3 +1,9 @@
 if __name__ == '__main__':
-    a = "Hello World"
-    print(a)
+    num = input("please input an integer: ")
+    num = int(num)
+    if num == 0:
+        print(num)
+    elif num > 0:
+        print(num, "is greater than 0")
+    else:
+        print(num, "is lower than 0")
